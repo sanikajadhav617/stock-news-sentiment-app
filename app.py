f4a3faf1e1d444ec4b3f0778e82c9ff7e071a882
sanikajadhav617 <jsanika144@gmail.com>
2026-01-19 18:35:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Hello! Stock News App is Working ✅"
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     app.run()
